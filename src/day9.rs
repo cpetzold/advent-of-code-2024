47f7fc504f4939f623b71b1e0aee4641913c7991
Conner Petzold <cpetzold@gmail.com>
2024-12-09 20:05:01 -0500

    day 9 part 2

--- src/day9.rs
+++ src/day9.rs
@@ -36,6 +36,39 @@ impl Disk {
         Disk { blocks }
     }
 
+    pub fn compact_whole_files(&self) -> Disk {
+        let mut blocks = self.blocks.clone();
+
+        for (_, file_blocks) in &blocks
+            .clone()
+            .iter()
+            .enumerate()
+            .rev()
+            .filter(|(_, b)| b.is_some())
+            .chunk_by(|(_, b)| **b)
+        {
+            let mut i = 0;
+            let mut file_blocks = file_blocks.collect_vec();
+            file_blocks.reverse();
+            let file_size = file_blocks.len();
+            let file_start_index = file_blocks[0].0;
+
+            while i < file_start_index && blocks[i..i + file_size].iter().any(|b| b.is_some()) {
+                i += 1;
+            }
+
+            if i >= file_start_index {
+                continue;
+            }
+
+            for (k, (j, _)) in file_blocks.iter().enumerate() {
+                blocks.swap(i + k, *j);
+            }
+        }
+
+        Disk { blocks }
+    }
+
     pub fn checksum(&self) -> usize {
         self.blocks
             .iter()
@@ -79,26 +112,51 @@ pub fn solve_part1(disk: &Disk) -> usize {
     compacted.checksum()
 }
 
+#[aoc(day9, part2)]
+pub fn solve_part2(disk: &Disk) -> usize {
+    let compacted = disk.compact_whole_files();
+    compacted.checksum()
+}
+
 #[cfg(test)]
 mod tests {
     use super::*;
 
     #[test]
     fn test_day9_input_generator() {
-        let input = "12345";
+        let input = "2333133121414131402";
 
         let disk = input_generator(&input);
 
-        assert_eq!(format!("{}", disk), "0..111....22222");
+        assert_eq!(
+            format!("{}", disk),
+            "00...111...2...333.44.5555.6666.777.888899"
+        );
     }
 
     #[test]
     fn test_day9_compact() {
-        let input = "12345";
+        let input = "2333133121414131402";
 
         let disk = input_generator(&input);
         let compacted = disk.compact();
-        assert_eq!(format!("{}", disk), "0..111....22222");
-        assert_eq!(format!("{}", compacted), "022111222......");
+        assert_eq!(
+            format!("{}", compacted),
+            "0099811188827773336446555566.............."
+        );
+        assert_eq!(compacted.checksum(), 1928);
+    }
+
+    #[test]
+    fn test_day9_compact_whole_files() {
+        let input = "2333133121414131402";
+
+        let disk = input_generator(&input);
+        let compacted = disk.compact_whole_files();
+        assert_eq!(
+            format!("{}", compacted),
+            "00992111777.44.333....5555.6666.....8888.."
+        );
+        assert_eq!(compacted.checksum(), 2858);
     }
 }
